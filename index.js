a2c2c5c8a3b1aa20f35f3f35c023d57c84b163dd
Andheden <axelort0607@gmail.com>
2025-01-31 13:24:21 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static("public"))
+
